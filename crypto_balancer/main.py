9779ee474344c4ffc20b01f8dd99b495bdcb42ac
Lars Voegtlin <lars.voegtlin@unifr.ch>
2020-05-17 15:40:42 +0200

    logging the output into a folder. The user can specify the folder where the logs will be saved

--- crypto_balancer/main.py
+++ crypto_balancer/main.py
@@ -1,43 +1,23 @@
 import argparse
 import configparser
+import itertools
 import logging
+import os
 import sys
+import time
+from multiprocessing import Pool, cpu_count
+from pathlib import Path
 
-from crypto_balancer.simple_balancer import SimpleBalancer
-from crypto_balancer.ccxt_exchange import CCXTExchange, exchanges
+from crypto_balancer.ccxt_exchange import CCXTExchange
 from crypto_balancer.executor import Executor
 from crypto_balancer.portfolio import Portfolio
+from crypto_balancer.simple_balancer import SimpleBalancer
 
 logger = logging.getLogger(__name__)
 
 
-def main(args=None):
-    portfolio_config = configparser.ConfigParser()
-    portfolio_config.read('portfolios.ini')
-
-    def portfolio_choices():
-        return set(portfolio_config.sections())
-
-    parser = argparse.ArgumentParser(
-        description='Balance holdings on an exchange.')
-    parser.add_argument('--trade', action="store_true",
-                        help='Actually place orders')
-    parser.add_argument('--force', action="store_true",
-                        help='Force rebalance')
-    parser.add_argument('--max_orders', default=5,
-                        help='Maximum number of orders to perform in '
-                             'rebalance')
-    parser.add_argument('--valuebase', default='USDT',
-                        help='Currency to value portfolio in')
-    parser.add_argument('--cancel', action="store_true",
-                        help='Cancel open orders first')
-    parser.add_argument('--mode', choices=['mid', 'passive', 'cheap'],
-                        default='mid',
-                        help='Mode to place orders')
-    parser.add_argument('portfolio', choices=portfolio_choices())
-    args = parser.parse_args()
-
-    portfolio_config = portfolio_config[args.portfolio]
+def balancing(portfolio_config, portfolio_name, trade, force, max_orders, valuebase, cancel, log_dir, mode):
+    set_up_logger(log_dir, portfolio_name)
 
     try:
         targets = [x.split() for x in portfolio_config['targets'].split('\n')]
@@ -52,102 +32,139 @@ def main(args=None):
                      .format(total_target))
         sys.exit(1)
 
-    valuebase = portfolio_config.get('valuebase') or args.valuebase
+    valuebase = portfolio_config.get('valuebase') or valuebase
 
     exchange = CCXTExchange(portfolio_config['exchange'],
                             targets.keys(),
                             portfolio_config['api_key'],
                             portfolio_config['api_secret'])
 
-    print("Connected to exchange: {}".format(exchange.name))
-    print()
+    logger.info("Connected to exchange: {}".format(exchange.name))
 
-    if args.cancel:
-        print("Cancelling open orders...")
+    if cancel:
+        logger.info("Cancelling open orders...")
         for order in exchange.cancel_orders():
-            print("Cancelled order:", order['symbol'], order['id'])
-        print()
+            logger.info("Cancelled order:", order['symbol'], order['id'])
 
     threshold = float(portfolio_config['threshold'])
-    max_orders = int(args.max_orders)
+    max_orders = int(max_orders)
 
     portfolio = Portfolio.make_portfolio(targets, exchange, threshold, valuebase)
 
-    print("Current Portfolio:")
+    logger.info("Current Portfolio:")
     for cur in portfolio.balances:
         bal = portfolio.balances[cur]
         pct = portfolio.balances_pct[cur]
         tgt = targets[cur]
-        print("  {:<6s} {:<8.2f} ({:>5.2f} / {:>5.2f}%)"
-              .format(cur, bal, pct, tgt))
+        logger.info("  {:<6s} {:<8.2f} ({:>5.2f} / {:>5.2f}%)".format(cur, bal, pct, tgt))
 
-    print()
-    print("  Total value: {:.2f} {}".format(portfolio.valuation_quote,
-                                            portfolio.quote_currency))
+    logger.info("  Total value: {:.2f} {}".format(portfolio.valuation_quote,
+                                                  portfolio.quote_currency))
     balancer = SimpleBalancer()
     executor = Executor(portfolio, exchange, balancer)
-    res = executor.run(force=args.force,
-                       trade=args.trade,
+    res = executor.run(force=force,
+                       trade=trade,
                        max_orders=max_orders,
-                       mode=args.mode)
+                       mode=mode)
 
-    print("  Balance RMS error: {:.2g} / {:.2g}".format(
+    logger.info("  Balance RMS error: {:.2g} / {:.2g}".format(
         res['initial_portfolio'].balance_rms_error,
         threshold))
 
-    print("  Balance Max error: {:.2g} / {:.2g}".format(
+    logger.info("  Balance Max error: {:.2g} / {:.2g}".format(
         res['initial_portfolio'].balance_max_error,
         threshold))
-    
-    print()
-    if not portfolio.needs_balancing and not args.force:
-        print("No balancing needed")
+
+    if not portfolio.needs_balancing and not force:
+        logger.info("No balancing needed")
         sys.exit(0)
 
-    print("Balancing needed{}:".format(" [FORCED]" if args.force else ""))
-    print()
-    print("Proposed Portfolio:")
+    logger.info("Balancing needed{}:".format(" [FORCED]" if force else ""))
+
+    logger.info("Proposed Portfolio:")
     portfolio = res['proposed_portfolio']
 
     if not portfolio:
-        print("Could not calculate a better portfolio")
+        logger.info("Could not calculate a better portfolio")
         sys.exit(0)
 
     for cur in portfolio.balances:
         bal = portfolio.balances[cur]
         pct = portfolio.balances_pct[cur]
         tgt = targets[cur]
-        print("  {:<6s} {:<8.2f} ({:>5.2f} / {:>5.2f}%)"
-              .format(cur, bal, pct, tgt))
+        logger.info("  {:<6s} {:<8.2f} ({:>5.2f} / {:>5.2f}%)"
+                    .format(cur, bal, pct, tgt))
 
-    print()
-    print("  Total value: {:.2f} {}".format(portfolio.valuation_quote,
-                                            portfolio.quote_currency))
-    print("  Balance RMS error: {:.2g} / {:.2g}".format(
+    logger.info("  Total value: {:.2f} {}".format(portfolio.valuation_quote,
+                                                  portfolio.quote_currency))
+    logger.info("  Balance RMS error: {:.2g} / {:.2g}".format(
         res['proposed_portfolio'].balance_rms_error,
         threshold))
 
-    print("  Balance Max error: {:.2g} / {:.2g}".format(
+    logger.info("  Balance Max error: {:.2g} / {:.2g}".format(
         res['proposed_portfolio'].balance_max_error,
         threshold))
 
     total_fee = '%s' % float('%.4g' % res['total_fee'])
-    print("  Total fees to re-balance: {} {}"
-          .format(total_fee,
-                  portfolio.quote_currency))
+    logger.info("  Total fees to re-balance: {} {}"
+                .format(total_fee, portfolio.quote_currency))
 
-    print()
-    print("Orders:")
-    if args.trade:
+    logger.info("Orders:")
+    if trade:
         for order in res['success']:
-            print("  Submitted: {}".format(order))
+            logger.info("  Submitted: {}".format(order))
 
         for order in res['errors']:
-            print("  Failed: {}".format(order))
+            logger.info("  Failed: {}".format(order))
     else:
         for order in res['orders']:
-            print("  " + str(order))
+            logger.info("  " + str(order))
+
+
+def set_up_logger(log_dir, portfolio_name):
+    log_path = Path(os.path.join(log_dir, portfolio_name))
+    log_path.mkdir(parents=True, exist_ok=True)
+    fh = logging.FileHandler(os.path.join(log_path, time.strftime('%d-%m-%y-%Hh-%Mm-%Ss') + '.txt'))
+    fh.setLevel(logging.INFO)
+    logger.addHandler(fh)
+    logger.setLevel(logging.INFO)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Balance holdings on an exchange.')
+    parser.add_argument('--trade', action="store_true",
+                        help='Actually place orders')
+    parser.add_argument('--force', action="store_true",
+                        help='Force rebalance')
+    parser.add_argument('--max_orders', default=5,
+                        help='Maximum number of orders to perform in '
+                             'rebalance')
+    parser.add_argument('--valuebase', default='USDT',
+                        help='Currency to value portfolio in')
+    parser.add_argument('--cancel', action="store_true",
+                        help='Cancel open orders first')
+    parser.add_argument('--log_dir', type=str,
+                        help='Path to the output folder',
+                        default='logs')
+    parser.add_argument('--mode', choices=['mid', 'passive', 'cheap'],
+                        default='mid',
+                        help='Mode to place orders')
+    args = parser.parse_args()
+
+    p_config = configparser.ConfigParser()
+    p_config.read('portfolios.ini')
+
+    pool = Pool(processes=cpu_count())
+    pool.starmap(balancing,
+                 zip([p_config[p] for p in p_config.sections()],
+                     p_config.sections(),
+                     itertools.repeat(args.trade),
+                     itertools.repeat(args.force),
+                     itertools.repeat(args.max_orders),
+                     itertools.repeat(args.valuebase),
+                     itertools.repeat(args.cancel),
+                     itertools.repeat(args.log_dir),
+                     itertools.repeat(args.mode)
+                     ))
+    pool.close()
